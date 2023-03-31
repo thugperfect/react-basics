825c4c297569e969b01174ae24d32f1c6c77cb7c
thugperfect <meenamani9444@gmail.com>
2023-03-31 16:47:27 +0000

    used useState insted of js fns

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Contentsnav from './components/Contentsnav';
 import Classname from './components/class';
-import Sidenav from './components/Sidenav';
+
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Contentsnav />
-      <Sidenav />
+    
 
 
     </div>
